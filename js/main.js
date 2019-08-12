8cc327e03877fa101ced162d16f84a1906973c94
Juan <juancarloscanoruiz7@gmail.com>
2019-08-12 16:06:58 -0500

    menú responsivo y header casi completo

--- js/main.js
+++ js/main.js
@@ -2,4 +2,16 @@ $(document).ready(function() {
   $(".home-slider").bxSlider({
     controls: false
   });
+
+  //menu responsive
+  const menu = document.querySelector(".menu-responsive");
+  console.log(menu);
+  const hamburguer = document.querySelector(".hamburguer-menu");
+  console.log(hamburguer);
+  menu.addEventListener("click", function(e) {
+    hamburguer.style.transform = "translate(0%)";
+    if (!hamburguer.is(e.target) && hamburguer.has(e.target).length === 0) {
+      console.log("hola");
+    }
+  });
 });
