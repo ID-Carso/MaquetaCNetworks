69e8f74cb484967845857788c6c7849796e3efe1
Juan Carlos <juancarloscanoruiz7@gmail.com>
2019-09-10 23:09:47 -0500

    Se cambiaron tamaños de textos, se ocultó menú hamburguesa y se puso la navbar, versión de escritorio ya en desarrollo

--- js/main.js
+++ js/main.js
@@ -147,7 +147,7 @@ $(document).ready(function() {
     </div>
     <div class="row no-gutters">
       <div class="col-md-4">
-        <p class="a-concert-subtitle">Conciertos</p>
+        <p class="a-concert-subtitle font-weight-bold">Conciertos</p>
         <p class="a-concert-subtitle">Entrevistas y mucho más</p>
       </div>
       <div class="col-md-8">
@@ -199,7 +199,7 @@ $(document).ready(function() {
         <img class="concert-footer-img concert-img-white" src="./images/home/tv-2.svg" alt="">
       </div>
       <div>
-        <p class="a-poster-text-white">
+        <p class="horario-text text-white">
           Horario sujetos a cambios
         </p>
       </div>
@@ -276,7 +276,7 @@ $(document).ready(function() {
       <img class="concert-footer-img" src="./images/home/tv-2.svg" alt="">
     </div>
     <div>
-      <p class="a-poster-text-black">
+      <p class="horario-text text-dark">
         Horario sujetos a cambios
       </p>
     </div>
@@ -301,8 +301,8 @@ $(document).ready(function() {
       <div class="slick-slider">
 
         <div class="row no-gutters">
-            <div class="col columnGap h-100">
-                <div class="height-second-poster h-100">
+            <div class="col columnGap">
+                <div class="height-second-poster">
                     <div class="poster-image pink-color">
                     </div>
                   <div class="a-vision-rectangle">
@@ -314,8 +314,8 @@ $(document).ready(function() {
               </div>
           </div>
         <div class="row no-gutters">
-          <div class="col columnGap h-100">
-              <div class="height-second-poster h-100">
+          <div class="col columnGap">
+              <div class="height-second-poster">
                 <div class="poster-image pink-color"></div>
                 <div class="image"></div>
                 <div class="a-vision-rectangle">
@@ -358,7 +358,7 @@ $(document).ready(function() {
               <img class="concert-footer-img" src="./images/home/tv-2.svg" alt="">
             </div>
             <div>
-              <p class="a-poster-text-black">
+              <p class="horario-text text-dark">
                 Horario sujetos a cambios
               </p>
             </div>
@@ -438,7 +438,7 @@ $(document).ready(function() {
         <img class="concert-footer-img concert-img-white" src="./images/home/tv-2.svg" alt="">
       </div>
       <div>
-        <p class="a-poster-text-white">
+        <p class="horario-text text-white">
           Horario sujetos a cambios
         </p>
       </div>
@@ -507,7 +507,7 @@ $(document).ready(function() {
           <img class="concert-footer-img" src="./images/home/tv-2.svg" alt="">
         </div>
         <div>
-          <p class="a-poster-text-black">
+          <p class="horario-text text-dark">
             Horario sujetos a cambios
           </p>
         </div>
@@ -516,22 +516,24 @@ $(document).ready(function() {
 
     var advertisingSection = `
     
-    <div class="row">
-      <div class="col-5 m-auto">
-        <div class="adv-headline-mb">
-          <h1 class="advertising-title">Disfruta</h1>
-          <h2 class="advertising-subtitle">La mejor programación</h2>
+    <div class="col-lg-10 m-auto">
+      <div class="row">
+        <div class="col-5 m-auto">
+          <div class="adv-headline-mb">
+            <h1 class="advertising-title">Disfruta</h1>
+            <h2 class="advertising-subtitle">La mejor programación</h2>
+          </div>
+          <div>
+            <p class="advertising-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tellus
+              tellus, rutrum sit amet purus in, interdum auctor nunc. Nulla
+              facilisis mauris vel elit finibus, sit amet ornare massa suscipit.
+            </p>
+          </div> 
         </div>
-        <div>
-          <p class="advertising-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tellus
-            tellus, rutrum sit amet purus in, interdum auctor nunc. Nulla
-            facilisis mauris vel elit finibus, sit amet ornare massa suscipit.
-          </p>
-        </div> 
-      </div>
-      <div class="col-7 no-gutters">
-        <img src="./images/home/planes-copy.png" class="advertising-image"/>
+        <div class="col-7 no-gutters">
+          <img src="./images/home/planes-copy.png" class="advertising-image"/>
+        </div>
       </div>
     </div>
     `;
