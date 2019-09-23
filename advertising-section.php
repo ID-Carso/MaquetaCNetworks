eef6731c1707476ae11645947a784cef1d0e98a0
Juan Carlos <juancarloscanoruiz7@gmail.com>
2019-09-22 19:29:40 -0500

    pantalla de claro canal casi completa

---
+++ advertising-section.php
@@ -0,0 +1,20 @@
+<div class="o-advertising-section">
+        <div class="row">
+            <div class="col-12 m-auto">
+                <div class="adv-headline-mb">
+                    <h1 class="advertising-title ">Disfruta</h1>
+                    <h2 class="advertising-subtitle">La mejor programación</h2>
+                </div>
+                <div>
+                    <p class="advertising-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tellus
+                        tellus, rutrum sit amet purus in, interdum auctor nunc. Nulla
+                        facilisis mauris vel elit finibus, sit amet ornare massa suscipit.
+                    </p>
+                </div>
+            </div>
+            <div class="col-12 no-gutters">
+                <img src="./images/home/planes-copy.png" class="advertising-image" />
+            </div>
+        </div>
+    </div>
---
+++ footer.php
@@ -0,0 +1,110 @@
+<footer>
+        <div class="o-footer-section">
+            <div class="row no-gutters terms-conditions-mobile">
+                <div class="col-10 col-md-10 mx-auto aviso-border">
+                    <div class="col-12 col-md-10 m-auto">
+                        <div class="logo-footer">
+                            <img src="./images/home/claro-grey.svg" alt="" class="claro-grey-img">
+                        </div>
+                        <p class="text-footer-regular">@2019 Claro tv. Todos los derechos reservados</p>
+                        <div class="d-flex justify-content-between">
+                            <div>
+                                <p class="text-footer-bold">Términos y condiciones</p>
+                            </div>
+                            <div>
+                                <p class="text-footer-bold">Aviso de privacidad</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--Términos y condiciones para desktop-->
+            <div class="row aviso-border terms-conditions-desktop align-items-end">
+                <div class="col-12 col-md-10 col-lg-6 col-xl-6">
+                    <div class="logo-footer">
+                        <img src="./images/home/claro-grey.svg" alt="" class="claro-grey-img" />
+                    </div>
+                    <p class="text-footer-regular text-footer-basic">
+                        @2019 Claro tv. Todos los derechos reservados
+                    </p>
+                    <div class="d-flex">
+                        <div>
+                            <a href="#">
+                                <p class="aviso-priv-blue">Términos y condiciones</p>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                <p class="aviso-priv-blue">Aviso de privacidad</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-xl-6">
+                    <p class="other-text-regular text-right text-footer-basic">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                    </p>
+                </div>
+            </div>
+            <!--Div logos de compañías-->
+            <div class="footer-companies">
+                <div class="d-flex margin-bottom-footer">
+                    <div class="col">
+                        <img class="d-block m-auto footer-img" src="./images/home/fast-icon.svg" alt="">
+                    </div>
+                    <div class="col">
+                        <img class="d-block m-auto footer-img" src="./images/home/claro-musica-icon.svg" alt="">
+                    </div>
+                    <div class="col">
+                        <img class="d-block m-auto footer-img" src="./images/home/claro-video-icon.svg" alt="">
+                    </div>
+                    <div class="col">
+                        <img class="d-block m-auto footer-img" src="./images/home/claro-musica-icon.svg" alt="">
+                    </div>
+                </div>
+                <div class="d-flex margin-bottom-footer">
+                    <div class="col">
+                        <img class="d-block m-auto small-footer-img" src="./images/home/sanborns-icon.svg" alt="">
+                    </div>
+                    <div class="col">
+                        <img class="d-block m-auto small-footer-img" src="./images/home/soumaya-icon.svg" alt="">
+                    </div>
+                    <div class="col">
+                        <img class="d-block m-auto small-footer-img" src="./images/home/claro-shop-icon.svg" alt="">
+                    </div>
+                    <div class="col">
+                        <img class="d-block m-auto small-footer-img" src="./images/home/fundacion-icon.svg" alt="">
+                    </div>
+                </div>
+            </div>
+            <!--Versión desktop de divs de logos de las compañías-->
+            <div class="footer-companies-desktop">
+                <div
+                    class="d-flex margin-bottom-footer col-lg-12 col-xl-12 m-auto justify-content-between align-items-center">
+                    <div class="col footer-border-right">
+                        <img class="d-block m-auto small-footer-img" src="./images/home/sanborns-icon.svg" alt="" />
+                    </div>
+                    <div class="col footer-border-right">
+                        <img class="d-block m-auto footer-img" src="./images/home/claro-musica-icon.svg" alt="" />
+                    </div>
+                    <div class="col footer-border-right">
+                        <img class="d-block m-auto footer-img" src="./images/home/claro-video-icon.svg" alt="" />
+                    </div>
+
+                    <div class="col footer-border-right">
+                        <img class="d-block m-auto footer-img" src="./images/home/fast-icon.svg" alt="" />
+                    </div>
+                    <div class="col footer-border-right">
+                        <img class="d-block m-auto small-footer-img" src="./images/home/soumaya-icon.svg" alt="" />
+                    </div>
+                    <div class="col footer-border-right">
+                        <img class="d-block m-auto small-footer-img" src="./images/home/claro-shop-icon.svg" alt="" />
+                    </div>
+                    <div class="col">
+                        <img class="d-block m-auto small-footer-img" src="./images/home/fundacion-icon.svg" alt="" />
+                    </div>
+                </div>
+            </div>
+            <!--Fin div logos de compañías-->
+        </div>
+    </footer>
