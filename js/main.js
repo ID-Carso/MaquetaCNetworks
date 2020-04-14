63eac308890a43e6abba74711ed2b4faf5f6f877
Juan Carlos <juancarloscanoruiz7@gmail.com>
2020-04-14 10:45:21 -0500

    se eliminaron algunas instrucciones innecesarias

--- js/main.js
+++ js/main.js
@@ -1880,24 +1880,6 @@ $(document).ready(function () {
   }
 
   /*** Dropdown del menú ***/
-  /*const dropdown = document.getElementsByClassName("sidebar-dropdown");
-  console.log(dropdown);
-
-  var i;
-
-  for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function() {
-      this.classList.toggle("dropdown-active");
-      var dropdownContent = this.nextElementSibling;
-      if (dropdownContent.style.display === "block") {
-        dropdownContent.style.animation = "hide-dropdown 0.5s";
-        dropdownContent.style.display = "none";
-      } else {
-        dropdownContent.style.display = "block";
-        dropdownContent.style.animation = "active-dropdown 0.5s";
-      }
-    });
-  }*/
 
   /*** Fin Dropdown del menú ***/
   recreateClickCalendar();
