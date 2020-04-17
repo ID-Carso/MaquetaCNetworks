37a250aa01e127deaaf6bac2818f181f27210bc6
annet-torres <60156224+annet-torres@users.noreply.github.com>
2020-04-17 15:27:58 -0500

    Update Progra_general.php

--- backoffice/Progra_general.php
+++ backoffice/Progra_general.php
@@ -12,54 +12,11 @@
 </head>
 <body>
     <main>
-        <?php
-        $nameButton = "Regresar a Administrar sitio";
-       // $nameSection = "Home";
-        include 'header-bo.php';
-       showHeader($nameButton, 1);
-        ?>
-        <div class="justify-content-center centro mx-auto position-title">
-        <span class="titulo">PROGRAMACIÓN GENERAL <br> DEL </span>
-        <span class="titulo-rojo">1ERO DE OCTUBRE-1ERO DE NOVIEMBRE</span>
-        </div>
-        <div class=" mr-5 float-right position-zona">
-        <label class="btn-zona">Zona horaria  <img src="../images/paises/chile.svg" class="Icon_paises1" /></label> 
-        </div>
-        <nav class="navbar-expand-sm  mt-2 position-nav">
-        <ul class="navbar-nav">
-      <li class="nav-item ">
-        <button class="btn-menu-select">Canal Claro</button>  
-        </li>
-        <li class="nav-item sp">
-        <button class="btn-menu-all">Concert Channel</button>  
-        </li>
-      <li class="nav-item sp">
-          <button class="btn-menu-all">Claro Cinema</button>
-          </li>
-      <li class="nav-item sp">
-          <button class="btn-menu-inac">Nuesta Visión</button>
-          </li>
-      <li class="nav-item sp ">
-          <button class="btn-menu-inac">Claro Sport</button>
-          </li>
-      <li class="nav-item sp">
-          <button class="btn-menu-all"> Home</button>
-         </li>
-    </ul>
-</nav>
-      <div class="mt-3 ml-5"> <span class="titulo-negrito">Última edición :</span>
-      <label class=" titulo-light separacion"> septiembre 17 2019 </label>
-      <label class="titulo-light">18:33:25</label>
-    </div>
-    <div class="float-right mr-5 ali">
-        <span class="titulo-negrito">Por :</span><label class=" titulo-light separacion">Antonio López Pérez </label> <label class="titulo-light">Usuario editor</label>
-    </div>
-    <div class=" float-right mt-2 ml-btn  mr-5 ">
-    <label class="btn-grilla mr-3">Grilla</label> 
-        <label class="btn-landing ">Landing</label>
-    </div>
+      <?
+      include 'Menu.php';
+      ?>
     <div class=" float-left ml-5 ali" >
-        <label class="btn-cargar mr-4"id="file">  Cargar archivos</label>  
+        <label class="btn-cargar mr-4"id="file" onClick="Verarchi()"> Cargar archivos</label>  
     </div>
     <div class="ml-2 ">
     <div class=" slidershow ali1">
@@ -81,7 +38,7 @@
     </div>
     <div class=" slidershow ali1 m-slider">
         <ul class="slider">
-        <li class="caption " style="display:block">
+        <li class="caption1 " style="display:block">
       <p class="separacion">MARTES 5</p>
     </li>
     <li class="caption">
@@ -98,7 +55,7 @@
     </div>
     </div>
   
-   
+   <div id="rempla">
     <div  id="tb1"class="contenedor-tabla  ml-5">
     <div class="contenedor-fila">
         <div class="contenedor-columna title-table">
@@ -131,6 +88,9 @@
         <div class="contenedor-columna title-table">
             <span> Imagenes</span>
          </div>
+         <div class="contenedor-columna title-table">
+             <span>Schedule Item Date Time</span>
+        </div>
         <div class="contenedor-columna title-table">
              <span>Schedule Item Long Date</span>
         </div>
@@ -195,6 +155,8 @@
         <div class="contenedor-columna"></div>
         <div class="contenedor-columna"></div>
         <div class="contenedor-columna"></div>
+        <div class="contenedor-columna"></div>
+        <div class="contenedor-columna"></div>
     </div>
 </div>
 <div class="contenedor ml-5">
@@ -202,7 +164,7 @@
         <div id="agregar" class="btn-crear mt-2 ml-2"><span class="signo"><p class="text-crea">Crear nueva entrada</p></span></div>
     </div>
 </div>
-
+</div>
     </main>
 </body>
 </html>
