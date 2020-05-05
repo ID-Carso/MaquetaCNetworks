3250175478bee6f7a70ad360f0e0d1743f57a4da
Annet Torres <annet-gomez7@gmail.com>
2020-05-04 22:35:28 -0500

    preload

--- backoffice/styles.php
+++ backoffice/styles.php
@@ -1,3 +1,9 @@
+<link rel="preload" as="font" crossorigin="crossorigin" type="font/woff2" href="./fonts/Montserrat-Black/Montserrat-Black.woff2">
+<link rel="preload" as="font" crossorigin="crossorigin" type="font/woff2" href="./fonts/Montserrat-Bold/Montserrat-Bold.woff2">
+<link rel="preload" as="font" crossorigin="crossorigin" type="font/woff2" href="./fonts/Montserrat-Medium/Montserrat-Medium.woff2">
+<link rel="preload" as="font" crossorigin="crossorigin" type="font/woff2" href="./fonts/Montserrat-Light/Montserrat-Light.woff2">
+<link rel="preload" as="font" crossorigin="crossorigin" type="font/woff2" href="./fonts/Montserrat-SemiBold/Montserrat-SemiBold.woff2">
+<link rel="preload" as="font" crossorigin="crossorigin" type="font/woff2" href="./fonts/Montserrat-Regular/Montserrat-Regular.woff2">
 <link rel="shortcut icon" type="image/png" href="./images/favicon/claro-fav.png" />
 <link rel="stylesheet"  href="./css/bootstrap-4.4.1/bootstrap.min.css" />
 <link rel="stylesheet" href="./css/style.css?t=<?php echo time(); ?>" />
