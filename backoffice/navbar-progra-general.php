77b93517817884dc9ac6a5c347d27df6c81a632b
Juan Carlos Cano Ruiz <juancarloscanoruiz7@gmail.com>
2020-05-13 10:05:59 -0500

    se empezó a maquetar navbar de programación general

---
+++ backoffice/navbar-progra-general.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+    include 'scripts.php';
+    include
+        'styles.php';
+    ?>
+</head>
+
+<body>
+    <div>
+        <p>CANAL CLARO</p>
+    </div>
+</body>
+
+</html>
