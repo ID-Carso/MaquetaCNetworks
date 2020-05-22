c76d6f0dd241098be98b5ab9e8b0f29857630c13
= <=>
2020-05-22 11:27:11 -0500

    se arregló problema de estilos en landing de canal claro

--- backoffice/prev/claro-canal.php
+++ backoffice/prev/claro-canal.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-  <?php include './prev/views/partials/head.php' ?>
+  <?php include './views/partials/head.php' ?>
 </head>
 
 <body>
@@ -15,9 +15,9 @@
       <div class="sidebar-content">
         <a href="home.php" class="sidebar-link">
           <div class="sidebar-item sidebar-border-bottom sidebar-border-top">
-         
-          <span class="dropdown-p">Inicio</span>
-         
+
+            <span class="dropdown-p">Inicio</span>
+
           </div>
         </a>
         <div class="sidebar-item sidebar-border-bottom sidebar-border-top">
@@ -75,7 +75,7 @@
     <div class="header">
       <div class="alert-user">
       </div>
-      
+
       <img src="./images/header/curva.svg" alt="" class="header-curve">
 
       <!--Menú para móvil -->
@@ -115,27 +115,27 @@
       <div class="today-container">
         <div class="row no-gutters landing-header">
           <div class="col-12 col-md-3 col-lg-3 col-xl-3 text-center text-md-left text-lg-left text-xl-left">
-          <div class="col-8 p-3 border-top border-left border-right border-bottom position-relative">
-          <img src="../images/pencil.svg" alt="" class="pencil">
-          <img src="./images/home/tv-1.svg" alt="" class="lading-header-image-claro"></div>
+            <div class="col-8 p-3 border-top border-left border-right border-bottom position-relative">
+              <img src="../images/pencil.svg" alt="" class="pencil">
+              <img src="./images/home/tv-1.svg" alt="" class="lading-header-image-claro"></div>
           </div>
           <div class="col-12 col-md-5 col-lg-5 col-xl-5">
             <div class="d-flex justify-content-center col-12 col-xl-12">
-                <div class="p-3 border-top border-left border-right border-bottom position-relative">
-                   <img src="../images/pencil.svg" alt="" class="pencil">
-                   <h1 class="a-today-claro-title">hoy en <span>canal claro</span></h1>
-                </div>
+              <div class="p-3 border-top border-left border-right border-bottom position-relative">
+                <img src="../images/pencil.svg" alt="" class="pencil">
+                <h1 class="a-today-claro-title">hoy en <span>canal claro</span></h1>
               </div>
+            </div>
           </div>
           <div class="col-12 col-md-4 col-lg-4 col-xl-4 text-center text-md-right text-lg-right text-xl-righ">
-          <a href="programacion.php">
-            <div class="d-flex float-right clearfix">
-               <div class=" p-3 border-top border-left border-right border-bottom position-relative">
-                    <img src="../images/pencil.svg" alt="" class="pencil">
-                   <button class="btn-claro-canal a-text-white-semibold btn-tomato">VER PROGRAMACIÓN</button>
-               </div>
-            </div>
-           </a>
+            <a href="programacion.php">
+              <div class="d-flex float-right clearfix">
+                <div class=" p-3 border-top border-left border-right border-bottom position-relative">
+                  <img src="../images/pencil.svg" alt="" class="pencil">
+                  <button class="btn-claro-canal a-text-white-semibold btn-tomato">VER PROGRAMACIÓN</button>
+                </div>
+              </div>
+            </a>
           </div>
         </div>
 
@@ -147,22 +147,24 @@
     <section class="next-week-claro">
       <div class="next-week-container">
 
-      <div class=" p-3 col-5 border-top border-left border-right border-bottom position-relative">
+        <div class=" p-3 col-5 border-top border-left border-right border-bottom position-relative">
           <img src="../images/pencil.svg" alt="" class="pencil ">
-        <h1 class="a-next-claro-title">próxima semana</h1></div>
+          <h1 class="a-next-claro-title">próxima semana</h1>
+        </div>
         <div class="p-3 col-5 border-top border-left border-right border-bottom position-relative">
           <img src="../images/pencil.svg" alt="" class="pencil">
-        <h2 class="a-next-claro-subtitle">
-          Ve un <span>adelanto</span> de la programación
-        </h2></div>
+          <h2 class="a-next-claro-subtitle">
+            Ve un <span>adelanto</span> de la programación
+          </h2>
+        </div>
 
         <div class="week-claro-video-container">
-        <div class=" p-3 border-top border-left border-right border-bottom position-relative">
-          <img src="../images/pencil.svg" alt="" class="pencil">
-          <video class="promo-video" autoplay muted controls loop>
-            <source src="http://www.claronetworks.openofficedospuntocero.info/video/canal-claro-promo.mp4" type="video/mp4">
-          </video>
-          <!--<div class="poster">
+          <div class=" p-3 border-top border-left border-right border-bottom position-relative">
+            <img src="../images/pencil.svg" alt="" class="pencil">
+            <video class="promo-video" autoplay muted controls loop>
+              <source src="http://www.claronetworks.openofficedospuntocero.info/video/canal-claro-promo.mp4" type="video/mp4">
+            </video>
+            <!--<div class="poster">
             <div class="next-week-thumb">
               <div class="a-next-week-rectangle">
                 <div class="poster-title-margin rectangle-content">
@@ -173,35 +175,39 @@
               </div>
             </div>
           </div>-->
+          </div>
         </div>
-</div>
 
       </div>
     </section>
     <section class="dont-lose-claro">
       <div class="dont-lose-container">
-      <div class=" p-3 col-5 border-top border-left border-right border-bottom position-relative">
+        <div class=" p-3 col-5 border-top border-left border-right border-bottom position-relative">
           <img src="../images/pencil.svg" alt="" class="pencil">
-        <h1 class="a-dontLose-claro-title">TIENES QUE VERLO</h1></div>
+          <h1 class="a-dontLose-claro-title">TIENES QUE VERLO</h1>
+        </div>
         <div class=" p-3 col-4 border-top border-left border-right border-bottom position-relative">
           <img src="../images/pencil.svg" alt="" class="pencil">
-        <h2 class="a-dontLose-claro-subtitle">
-          <!-- ve un <span>adelanto</span> de la programación -->
-          ¡No te lo puedes perder!
-        </h2></div>
+          <h2 class="a-dontLose-claro-subtitle">
+            <!-- ve un <span>adelanto</span> de la programación -->
+            ¡No te lo puedes perder!
+          </h2>
+        </div>
         <div class="dontLose-video-container">
           <div class="section-slider" id="dontLose-claro-slider">
           </div>
           <div>
-          <div class="col-6 border-top border-left border-right border-bottom position-relative">
-          <img src="../images/pencil.svg" alt="" class="pencil">
-            <h1 class="a-dontLose-claro-title">Contenido exclusivo</h1></div>
+            <div class="col-6 border-top border-left border-right border-bottom position-relative">
+              <img src="../images/pencil.svg" alt="" class="pencil">
+              <h1 class="a-dontLose-claro-title">Contenido exclusivo</h1>
+            </div>
             <div class="col-7 pt-3 border-top border-left border-right border-bottom position-relative">
-          <img src="../images/pencil.svg" alt="" class="pencil">
-            <h2 class="a-dontLose-claro-subtitle">
-              <!-- ve un <span>adelanto</span> de la programación -->
-              Únete y disfruta nuestras producciones propias.
-            </h2></div>
+              <img src="../images/pencil.svg" alt="" class="pencil">
+              <h2 class="a-dontLose-claro-subtitle">
+                <!-- ve un <span>adelanto</span> de la programación -->
+                Únete y disfruta nuestras producciones propias.
+              </h2>
+            </div>
 
             <div class="section-slider dontLose-claro-slider" id="exlusiveContentClaroSlider">
             </div>
@@ -218,39 +224,39 @@
       <div class="row no-gutters">
         <div class="col-12">
           <div class=" d-flex justify-content-center pt-5 ">
-                <div class=" p-3 border-top border-left border-right border-bottom position-relative">
-                    <img src="../images/pencil.svg" alt="" class="pencil">
-                     <h1 class="footer-title-claro">¡síguenos!</h1>
-                 </div>
-        </div>
+            <div class=" p-3 border-top border-left border-right border-bottom position-relative">
+              <img src="../images/pencil.svg" alt="" class="pencil">
+              <h1 class="footer-title-claro">¡síguenos!</h1>
+            </div>
+          </div>
         </div>
         <div class="social-media">
           <div class="col ">
             <a href="https://www.facebook.com/CanalClaro/" target="_blank">
-            <div class="p-3 border-top border-left border-right border-bottom position-relative">
-          <img src="../images/pencil.svg" alt="" class="pencil">
-              <img class="social-icon" src="./images/redes/facebook-icon-red.svg" alt="" /></div>
+              <div class="p-3 border-top border-left border-right border-bottom position-relative">
+                <img src="../images/pencil.svg" alt="" class="pencil">
+                <img class="social-icon" src="./images/redes/facebook-icon-red.svg" alt="" /></div>
             </a>
           </div>
           <div class="col">
             <a href="https://www.instagram.com/canalclaro/?hl=es-la" target="_blank">
-            <div class="p-3 border-top border-left border-right border-bottom position-relative">
-          <img src="../images/pencil.svg" alt="" class="pencil">
-              <img class="social-icon" src="./images/redes/insta-icon-red.svg" alt="" /></div>
+              <div class="p-3 border-top border-left border-right border-bottom position-relative">
+                <img src="../images/pencil.svg" alt="" class="pencil">
+                <img class="social-icon" src="./images/redes/insta-icon-red.svg" alt="" /></div>
             </a>
           </div>
           <div class="col">
             <a href="https://twitter.com/canalclaro" target="_blank">
-            <div class="p-3 border-top border-left border-right border-bottom position-relative">
-          <img src="../images/pencil.svg" alt="" class="pencil">
-              <img class="social-icon" src="./images/redes/twitter-icon-red.svg" alt="" /></div>
+              <div class="p-3 border-top border-left border-right border-bottom position-relative">
+                <img src="../images/pencil.svg" alt="" class="pencil">
+                <img class="social-icon" src="./images/redes/twitter-icon-red.svg" alt="" /></div>
             </a>
           </div>
           <div class="col">
             <a href="https://www.youtube.com/user/CanalClaroTV" target="_blank">
-            <div class="p-3 border-top border-left border-right border-bottom position-relative">
-          <img src="../images/pencil.svg" alt="" class="pencil">
-              <img class="social-icon" src="./images/redes/youtube-icon-red.svg" alt="" /></div>
+              <div class="p-3 border-top border-left border-right border-bottom position-relative">
+                <img src="../images/pencil.svg" alt="" class="pencil">
+                <img class="social-icon" src="./images/redes/youtube-icon-red.svg" alt="" /></div>
             </a>
           </div>
         </div>
