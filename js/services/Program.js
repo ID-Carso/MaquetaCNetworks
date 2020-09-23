a097f65fea449eca8ee53146d2273fd8ee6111eb
zaid-srt <asolanosrt@outlook.com>
2020-09-23 08:15:14 -0500

    Cambios

--- js/services/Program.js
+++ js/services/Program.js
@@ -92,10 +92,13 @@ function destroySlider(container) {
 
 function getPrograms(date, country) {
     let dataProgram = {
-        function: "getPrograms",
+        // function: "getPrograms",
+        function: "getProgramsGMT",
         date,
         country,
     };
+
+    console.log(country, date);
 
     //NORMAL
     let nowSliderCanalClaro = $(".today-claro-slider");
